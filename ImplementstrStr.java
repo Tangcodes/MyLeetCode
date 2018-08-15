66419af0cf7ca9925e6e1b62cd621581eb3a35fe
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2018-08-14 22:02:09 -0500

    28. Implement strStr()

---
+++ ImplementstrStr.java
@@ -0,0 +1,21 @@
+package implementstrstr;
+
+public class ImplementstrStr {
+    
+    public static int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        String haystack = new String("hello");
+        int ans = strStr(haystack,"ll" );
+        System.out.print(ans);
+    }
+}
