147c599f59e27e518a0f9b607a06f0d941fea5b3
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2018-08-24 11:32:44 -0500

    73. Set Matrix Zeroes

---
+++ SetMatrixZeroes.java
@@ -0,0 +1,36 @@
+package setmatrixzeroes;
+
+import java.util.Arrays;
+
+public class SetMatrixZeroes {
+    public static void setZeroes(int[][] matrix) {
+        int col0=1;
+        int row=matrix.length;
+        int col=matrix[0].length;
+        
+        for (int i=0;i<row;i++){
+            if (matrix[i][0]==0) col0=0;
+            for (int j=1;j<col;j++){
+                if (matrix[i][j]==0){  
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            } 
+        }
+        for (int i=row-1;i>=0;i--){
+            for (int j=col-1;j>=1;j--){
+                if (matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0; 
+                }
+            }
+            if (col0 == 0) matrix[i][0] = 0;
+        }  
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        setZeroes(matrix);
+        System.out.print(Arrays.deepToString(matrix));
+    }
+    
+}
