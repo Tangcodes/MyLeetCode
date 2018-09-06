388964de4314bebc92b235fd34ec348c3a4bf844
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2018-09-05 21:14:53 -0500

    350. Intersection of Two Arrays II

---
+++ IntersectionofTwoArraysII.java
@@ -0,0 +1,44 @@
+
+package intersectionoftwoarraysii;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class IntersectionofTwoArraysII {
+    
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i < nums1.length; i++)
+        {
+            if(map.containsKey(nums1[i])) map.put(nums1[i], map.get(nums1[i])+1);
+            else map.put(nums1[i], 1);
+        }
+    
+        for(int i = 0; i < nums2.length; i++)
+        {
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0)
+            {
+                result.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i])-1);
+            }
+        }
+    
+       int[] r = new int[result.size()];
+       for(int i = 0; i < result.size(); i++)
+       {
+           r[i] = result.get(i);
+       }
+    
+       return r;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+        int[] ans = intersect(nums1,nums2);
+        System.out.println(Arrays.toString(ans));
+    }
+    
+}
