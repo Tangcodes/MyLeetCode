762ce6abc451a2ef36085840f3d159edfb931609
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2018-09-22 14:47:14 -0500

    416. Partition Equal Subset Sum

---
+++ Linkedin/PartitionEqualSubsetSum.java
@@ -0,0 +1,46 @@
+
+package partitionequalsubsetsum;
+
+import java.util.Arrays;
+
+public class PartitionEqualSubsetSum {
+    
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+    
+        for (int num : nums) {
+            sum += num;
+        }
+    
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        sum /= 2;
+
+        int n = nums.length;
+        boolean[][] dp = new boolean[n+1][sum+1];
+    
+        dp[0][0] = true;
+    
+        for (int i = 1; i < n+1; i++) {
+            dp[i][0] = true;
+        }
+    
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j >= nums[i-1]) {
+                    dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
+                }
+            }
+        }
+   
+        return dp[n][sum];
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 11, 5};
+        System.out.println(canPartition(nums));
+    }
+    
+}
