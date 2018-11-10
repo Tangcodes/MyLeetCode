24e8c3d297b88dae9ea25ebadb29484e9ddb617d
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2018-11-09 22:53:31 -0600

    Update MaximizeDistancetoClosestPerson.java

--- Google/MaximizeDistancetoClosestPerson.java
+++ Google/MaximizeDistancetoClosestPerson.java
@@ -7,29 +7,30 @@ import java.util.Arrays;
 public class MaximizeDistancetoClosestPerson {
 
     public static int maxDistToClosest(int[] seats) {
-        int[] dis = new int[seats.length];
-        dis[0]=-1;
-        for (int i=0; i<seats.length;i++){
-            if (seats[i]==1) {
-                dis[i]=-1;
-            }
-            else{
-                if (i==0) {
-                    continue;
-                }
-                dis[i] = dis[i-1]+1;
-            }
-        }
-        
-        for (int i=seats.length-2; i>=0;i--){
-            if (dis[i]==-1) continue;
-            else{
-                dis[i] = dis[i+1]==-1? Math.min(1, dis[i]): Math.min(dis[i],dis[i+1]+1);
+        int N = seats.length;
+        int K = 0; //current longest group of empty seats
+        int ans = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) {
+                K = 0;
+            } else {
+                K++;
+                ans = Math.max(ans, (K + 1) / 2);
             }
         }
-        Arrays.sort(dis);
-        //return dis[dis.length-1]==-1?1:dis[dis.length-1];
-        return dis[dis.length-1];
+
+        for (int i = 0; i < N; ++i)  if (seats[i] == 1) {
+            ans = Math.max(ans, i);
+            break;
+        }
+
+        for (int i = N-1; i >= 0; --i)  if (seats[i] == 1) {
+            ans = Math.max(ans, N - 1 - i);
+            break;
+        }
+
+        return ans;
     }
     
     public static void main(String[] args) {
