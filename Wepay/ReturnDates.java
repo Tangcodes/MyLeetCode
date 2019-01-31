8e1b6fd4e3c0ccf9d3d8c19a0ac54af5fa2c0aa7
Tangcodes <36091676+Tangcodes@users.noreply.github.com>
2019-01-31 17:46:05 -0600

    Update ReturnDates.java

--- Wepay/ReturnDates.java
+++ Wepay/ReturnDates.java
@@ -6,10 +6,13 @@ public class ReturnDates {
     
     public static void main(String[] args) {
         Calendar calendar=Calendar.getInstance();
-        calendar.set(Calendar.YEAR,2019);
-        calendar.set(Calendar.MONTH,0);
-        calendar.set(Calendar.WEEK_OF_MONTH,3);
-        calendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
-        System.out.println((calendar.get(Calendar.DATE)));
+        calendar.set(Calendar.YEAR,2018);
+        calendar.set(Calendar.MONTH,9);
+        calendar.set(Calendar.DATE,1);
+	
+        while (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.TUESDAY){
+            calendar.add(Calendar.DATE,1);
+	}
+        System.out.println(calendar.get(Calendar.DATE) + 7);
     }
 }
